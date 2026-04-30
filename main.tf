366c683ee97322bae28049eaa97595e61a50d3db
EC2 Default User <ec2-user@ip-172-31-73-220.ec2.internal>
2026-04-30 01:22:58 +0000

    correçao da vulnerabilidades

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "terraform_vpc" {
 
 # Correcao primeira issue
 resource "aws_flow_log" "example" {
-  log_destination      = "arn:aws:s3:::<SEU-BUCKET-NAME>"
+  log_destination      = "arn:aws:s3:::clc15-willian-terraform"
   log_destination_type = "s3"
   traffic_type         = "ALL"
   vpc_id               = aws_vpc.terraform_vpc.id
