dccde4fe36bd86df9344ed5a7abc79c834464af0
EC2 Default User <ec2-user@ip-172-31-73-220.ec2.internal>
2026-04-30 01:18:04 +0000

    correçao da vulnerabilidades

--- main.tf
+++ main.tf
@@ -9,6 +9,23 @@ resource "aws_vpc" "terraform_vpc" {
   }
 }
 
+# Correcao primeira issue
+resource "aws_flow_log" "example" {
+  log_destination      = "arn:aws:s3:::<SEU-BUCKET-NAME>"
+  log_destination_type = "s3"
+  traffic_type         = "ALL"
+  vpc_id               = aws_vpc.terraform_vpc.id
+}
+
+# Correcao segunda issue
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.terraform_vpc.id
+  
+  tags = {
+    Name = "my-iac-sg"
+  }
+}
+
 # CRIA AS 4 SUBNETS EM SUAS RESPECTIVAS AZs ##
 
 resource "aws_subnet" "subnet_public_1a" {
